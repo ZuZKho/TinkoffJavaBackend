b80a15b7770598ea6069b7af44f3a10fd810178a
Matvey <matveymag@mail.ru>
2024-02-18 19:55:09 +0700

    initial commit

---
+++ bot/src/main/java/edu/java/bot/UserMessageProcessor.java
@@ -0,0 +1,39 @@
+package edu.java.bot;
+
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import edu.java.bot.commands.CommandInterface;
+import edu.java.bot.commands.HelpCommand;
+import edu.java.bot.commands.ListCommand;
+import edu.java.bot.commands.StartCommand;
+import edu.java.bot.commands.TrackCommand;
+import edu.java.bot.commands.UntrackCommand;
+import java.util.List;
+
+public class UserMessageProcessor {
+
+    public static List<? extends CommandInterface> commands() {
+        return List.of(
+            new HelpCommand(),
+            new StartCommand(),
+            new ListCommand(),
+            new TrackCommand(),
+            new UntrackCommand()
+        );
+    }
+
+    public static SendMessage process(Update update) {
+        if (update.message() == null) {
+            return null;
+        }
+
+        for (var command : commands()) {
+            if (command.supports(update)) {
+                return command.handle(update);
+            }
+        }
+
+        var chatId = update.message().chat().id();
+        return new SendMessage(chatId, "No such command, use /help");
+    }
+}
